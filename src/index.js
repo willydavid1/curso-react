19fdc05cbfe51159196148fa519f197b34afc58a
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-12-03 11:45:41 -0500

    1- clase ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+// // siempre hemos podido crear elementos con document.createElement("que tipo de elemento queremos crear")
+// const element = document.createElement('h1'); //creamos un elemento
+// element.innerText = 'Hello, platzi Badges!'; //a ese elemento le damos un texto
 
-// const container = document.getElementById('app');
+// //donde queremos poner ese elemento, en la carpeta publica hay un index.html que hay un unico elemento llamado app
+// const container = document.getElementById('app'); //obtenemos el elemento app (es un div) del index.html
 
-// container.appendChild(element);
+// container.appendChild(element); //añadimos al container el elemento | tenemos un elemento h1 - un contenedor - y lo presentamos en pantalla SIN USAR REACT
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+// HAREMOS LO MISMO USANDO REACT, importamos React del modulo react igual para el reactDOM
+import React from 'react'; //React como análogo a createElement
+import ReactDOM from 'react-dom'; //ReactDOM a appendChild
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hola, Platzi badges from react</h1>; //creamos un elemento h1
 
-const container = document.getElementById('app');
+const container = document.getElementById('app'); // obtenemos el elemento contenedor con id app
 
-// ReactDOM.render(__qué__, __dónde__);
+//lo que no vamos a hacer igual que arriba es el appendChild(element) añadir al container el elemento
+// ReactDOM.render(__qué__, __dónde__) lo queremos renderizar
 ReactDOM.render(element, container);
