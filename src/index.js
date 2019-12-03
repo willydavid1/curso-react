5df6e749466dbfe60add15740e145bff46b951a7
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-12-03 16:23:41 -0500

    2clase_JSX

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
-// // siempre hemos podido crear elementos con document.createElement("que tipo de elemento queremos crear")
-// const element = document.createElement('h1'); //creamos un elemento
-// element.innerText = 'Hello, platzi Badges!'; //a ese elemento le damos un texto
-
-// //donde queremos poner ese elemento, en la carpeta publica hay un index.html que hay un unico elemento llamado app
-// const container = document.getElementById('app'); //obtenemos el elemento app (es un div) del index.html
-
-// container.appendChild(element); //añadimos al container el elemento | tenemos un elemento h1 - un contenedor - y lo presentamos en pantalla SIN USAR REACT
-
-// HAREMOS LO MISMO USANDO REACT, importamos React del modulo react igual para el reactDOM
+// HAREMOS LO MISMO USANDO REACT, importamos React del modulo react igual para el reactDOM | tenemos un elemento h1 - un contenedor - y lo presentamos en pantalla SIN USAR REACT
 import React from 'react'; //React como análogo a createElement
 import ReactDOM from 'react-dom'; //ReactDOM a appendChild
 
-const element = <h1>Hola, Platzi badges from react</h1>; //creamos un elemento h1
+// const jsx = <h1>Hola, Platzi badges from react</h1>; //creamos un elemento h1
+// const element = React.createElement(
+// 	'a',
+// 	{ href: 'https://platzi.com' },
+// 	'Ir a platzi'
+// ); //crea ese elemento es lo mismo de arriba
 
+// const name = 'willy david';
+// const element = React.createElement('h1', {}, `Hola soy ${name}`); //insertamos una variable
+
+// const sum = () => 3 + 3;
+
+//como hacemos esto con JSX insertar variables en un string o ejecutar expresiones de js se coloca entre llaves {}
+// const jsx = <h1>Hola soy, {sum()}</h1>; //si la expresion es false, 0, null, undefined no se mostrara
+var id = "hola"
+//aqui definimos un div y dentro contenido es la ventaja de jsx comparado con React.createElement
+const jsx = (
+	<div>
+		<h1>Hola, soy Willy</h1>
+		<p>Soy ingeniero frontend</p>
+	</div>
+);
+const element = React.createElement( //definimos lo de arribaJSX con React.createElement
+	'div',
+	{},
+	React.createElement('h1', {}, 'Hola, soy Willy'),
+	React.createElement('p', {}, 'Soy ingeniero de la web')
+);
 const container = document.getElementById('app'); // obtenemos el elemento contenedor con id app
 
 //lo que no vamos a hacer igual que arriba es el appendChild(element) añadir al container el elemento
 // ReactDOM.render(__qué__, __dónde__) lo queremos renderizar
 ReactDOM.render(element, container);
+
+
+// Aprendimos que es JSX, lo escribimos le anidamos elementos es decir crear un div y dentro un h1 y INSERTAMOS VARIABLES, QUE SON EXPRESIONES DE JS con {expresion} y lo comparamos con React.createElement 
