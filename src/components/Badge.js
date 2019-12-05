ba5b1ff195919527ad6472fcf8c08c65d20b9d12
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-12-05 10:06:31 -0500

    4-Estilos (a componentes y globales - import - className)

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,5 +1,7 @@
 import React from 'react'; //importamos react porque estaremos utilizando jsx
 
+import './styles/Badge.css'; //No guardamos en variable los estilos sino los importamos directamente y puede ser usado dentro y acceder a cualquier estilo
+
 import confLogo from '../images/badge-header.svg'; //importamos la imagen y accedemos a ella como confLogo
 
 //creamos el componente Badge, Los  componentes son clases
@@ -7,25 +9,29 @@ class Badge extends React.Component {
 	render() {
 		// el render define el resultado que vamos a ver en pantalla, cuando hagamos render retornaremos algo
 		return (
-			<div>
-				<div>
+			<div className="Badge">
+				<div className="Badge__header">
 					<img src={confLogo} alt="Logo de la conferencia" />
 				</div>
 
-				<div>
-					<img src="https://es.gravatar.com/avatar?d=identicon" alt="avatar" />
+				<div className="Badge__section-name">
+					<img
+						className="Badge__avatar"
+						src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
+						alt="avatar"
+					/>
 
 					<h1>
 						Willy <br /> Daconceicao
 					</h1>
 				</div>
 
-				<div>
-					<p>Ingeniero frontend</p>
-					<p>@willydavidlozada</p>
+				<div className="Badge__section-info">
+					<h3>Ingeniero frontend</h3>
+					<div>@willydavidlozada</div>
 				</div>
 
-				<div>#platziconf</div>
+				<div className="Badge__footer">#platziconf</div>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import Badge from "./components/Badge"; //importaremos la clase badge de la carpeta componentes
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Badge from './components/Badge'; //importaremos la clase badge de la carpeta componentes
+import 'bootstrap/dist/css/bootstrap.css'; //importaremos bootstrap, pero hay que instalarlo con  npm install bootstrap y estos estilos los tiene el componente
+import './global.css'; //son estilos de css globales que estan en esta misma carpeta src
 
 const container = document.getElementById('app'); // obtenemos el elemento contenedor con id app
 
 // ReactDOM.render(__qué__, __dónde__) lo queremos renderizar
-ReactDOM.render(<Badge />, container); // cuando el "que vamos a renderizar" no tiene el <> es un componente pero a ReactDOM.render() hay que darle un elemento asi <Badge /> 
+ReactDOM.render(<Badge />, container); // cuando el "que vamos a renderizar" no tiene el <> es un componente pero a ReactDOM.render() hay que darle un elemento asi <Badge />
