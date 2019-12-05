e144cbbae5cf49de1c0075fb8c74a4cf359443bc
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-12-05 11:17:46 -0500

    5-Props (this.props.VALOR los recibe el componente)

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -8,6 +8,7 @@ import confLogo from '../images/badge-header.svg'; //importamos la imagen y acce
 class Badge extends React.Component {
 	render() {
 		// el render define el resultado que vamos a ver en pantalla, cuando hagamos render retornaremos algo
+		// PODEMOS DECIR QUE LOS PROPS QUE SE MANDAN DESDE EL ELEMENTO, LOS RECIBE EL COMPONENTE CON this.props.lastName
 		return (
 			<div className="Badge">
 				<div className="Badge__header">
@@ -17,18 +18,18 @@ class Badge extends React.Component {
 				<div className="Badge__section-name">
 					<img
 						className="Badge__avatar"
-						src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
+						src={this.props.avatarUrl}
 						alt="avatar"
 					/>
 
 					<h1>
-						Willy <br /> Daconceicao
+						{this.props.firstName} <br /> {this.props.lastName}
 					</h1>
 				</div>
 
 				<div className="Badge__section-info">
-					<h3>Ingeniero frontend</h3>
-					<div>@willydavidlozada</div>
+					<h3>{this.props.jobTitle}</h3>
+					<div>@{this.props.twitter}</div>
 				</div>
 
 				<div className="Badge__footer">#platziconf</div>
--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ import './global.css'; //son estilos de css globales que estan en esta misma car
 const container = document.getElementById('app'); // obtenemos el elemento contenedor con id app
 
 // ReactDOM.render(__qué__, __dónde__) lo queremos renderizar
-ReactDOM.render(<Badge />, container); // cuando el "que vamos a renderizar" no tiene el <> es un componente pero a ReactDOM.render() hay que darle un elemento asi <Badge />
+ReactDOM.render(
+	<Badge
+		firstName="willy"
+		lastName="daconceicao"
+		avatarUrl="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
+		jobTitle="Frontend Engineer"
+		twitter="WillyDaconceic1"
+	/>,
+	container
+); // renderiza este componente con estas propiedades, Y en el componente recibe this.props.firstName y this.props.lastName y lo que pongamos aca lo pone alla
