65b2ce77b938744bf1740a7ff456fe5a899d1495
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-12-09 17:37:24 -0500

    9- levantar estado (poner estado donde se pueda pasar como props a componentes)

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,22 +1,20 @@
 import React from 'react'; //necesitamos importar react porque usaremos componentes
 
 class BadgeForm extends React.Component {
-	state = {
-		jobTitle: 'Designer'
-	};
+	// state = {};
 
-	handleChange = (e) => {
-		//obtenemos el evento en este metodo de la clase (manejamos el cambio el evento) y obtenemos el valor de lo que estamos escribiendo y el name del input que causa este evento, y lo guardamos todo en un objeto literal
-		// console.log({
-		// 	name: e.target.name,
-		// 	value: e.target.value
-		// });
+	// handleChange = (e) => {
+	// 	//obtenemos el evento en este metodo de la clase (manejamos el cambio el evento) y obtenemos el valor de lo que estamos escribiendo y el name del input que causa este evento, y lo guardamos todo en un objeto literal
+	// 	// console.log({
+	// 	// 	name: e.target.name,
+	// 	// 	value: e.target.value
+	// 	// });
 
-		// guardamos la informacion en el estado y se le pasa un objeto con info a guardar, en este caso guardamos en el estado un objeto con un atributo que guarda lo que escribe el usuario | para leerlo se utiliza this.state.firstName
-		this.setState({
-			[e.target.name]: e.target.value //cada uno de sus campos esta guardando su info en el estado con atributos diferentes
-		});
-	};
+	// 	// guardamos la informacion en el estado y se le pasa un objeto con info a guardar, en este caso guardamos en el estado un objeto con un atributo que guarda lo que escribe el usuario | para leerlo se utiliza this.state.firstName
+	// 	this.setState({
+	// 		[e.target.name]: e.target.value //cada uno de sus campos esta guardando su info en el estado con atributos diferentes
+	// 	});
+	// };
 
 	handleClick = (e) => {
 		// manejamos el evento click del boton
@@ -39,55 +37,55 @@ class BadgeForm extends React.Component {
 					<div className="form-group">
 						<label>First Name</label>
 						<input
-							onChange={this.handleChange}
+							onChange={this.props.onChange}
 							className="form-control"
 							type="text"
 							name="firstName"
-							value={this.state.firstName}
+							value={this.props.formValues.firstName}
 						/>
 					</div>
 
 					<div className="form-group">
 						<label>Last Name</label>
 						<input
-							onChange={this.handleChange}
+							onChange={this.props.onChange}
 							className="form-control"
 							type="text"
 							name="lastName"
-							value={this.state.lastName}
+							value={this.props.formValues.lastName}
 						/>
 					</div>
 
 					<div className="form-group">
 						<label>email</label>
 						<input
-							onChange={this.handleChange}
+							onChange={this.props.onChange}
 							className="form-control"
 							type="email"
 							name="email"
-							value={this.state.email}
+							value={this.props.formValues.email}
 						/>
 					</div>
 
 					<div className="form-group">
 						<label>Job title</label>
 						<input
-							onChange={this.handleChange}
+							onChange={this.props.onChange}
 							className="form-control"
 							type="text"
 							name="jobTitle"
-							value={this.state.jobTitle}
+							value={this.props.formValues.jobTitle}
 						/>
 					</div>
 
 					<div className="form-group">
 						<label>Twitter</label>
 						<input
-							onChange={this.handleChange}
+							onChange={this.props.onChange}
 							className="form-control"
 							type="text"
 							name="twitter"
-							value={this.state.twitter}
+							value={this.props.formValues.twitter}
 						/>
 					</div>
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,31 @@ import Badge from '../components/Badge'; //importamos el componente Badge - reci
 import BadgeForm from '../components/BadgeForm'; //importamos el componente BadgeForm de la carpeta de componentes y hacemos referencia a el con BadgeForm
 
 class BadgeNew extends React.Component {
+	state = {
+		form: {
+			//inicializamos el estado de BadgeNew y le añadimos una propiedad que tiene un objeto inicializando los valores del formulario, para evitar el warning en la consola
+			firstName: '',
+			lastName: '',
+			email: '',
+			jobTitle: '',
+			twitter: ''
+		}
+	};
+
+	// handleChange lo tiene ahora este componente y ya no badgeForm y para obtenerlo se le pasa como props al componente|creamos un metodo que recibe el evento y cuando lo reciba va a hacer un setState y que el atributo form tenga el valor del input que acaba de ocurrir
+	handleChange = (e) => {
+		// const nextForm = this.state.form; //almacemamos en nextForm el objeto form
+		// nextForm[e.target.name] = e.target.value; //creamos un valor dentro de ese objeto form
+
+		//sobreescribe el form cuando ocurra un nuevo evento para evitarlo podemos hacer una copia del estado del form o desplegamos los valores que tenga el objeto form y le añadimos uno nuevo
+		this.setState({
+			form: {
+				...this.state.form, //desplegamos el objeto (el estado) y le añadimos uno nuevo atributo o sobreescribimos el que ya estaba guardado
+				[e.target.name]: e.target.value //del evento del input tomas el name y le asignas como valor, lo que se escriba en el input
+			}
+		});
+	};
+
 	render() {
 		return (
 			<div>
@@ -19,16 +44,20 @@ class BadgeNew extends React.Component {
 					<div className="row">
 						<div className="col-6">
 							<Badge
-								firstName="Willy"
-								lastName="Daconceicao"
-								twitter="WillyDaconceic1"
-								jobTitle="Frontend Engineer"
+								firstName={this.state.form.firstName}
+								lastName={this.state.form.lastName}
+								twitter={this.state.form.twitter}
+								jobTitle={this.state.form.jobTitle}
+								email={this.state.form.email}
 								avatarUrl="https://willydavid1.github.io/images/mi-foto-recortada.jpg"
 							/>
 						</div>
 
 						<div className="col-6">
-							<BadgeForm />
+							<BadgeForm
+								onChange={this.handleChange}
+								formValues={this.state.form}
+							/>
 						</div>
 					</div>
 				</div>
