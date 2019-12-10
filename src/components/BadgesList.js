67ff0b6806a247076e37b153026eccf42bd99739
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2019-12-10 11:55:50 -0500

    10- lista de componentes (listamos lo que esta en estado)

---
+++ src/components/BadgesList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './styles/estilosBadgesList.css'; //IMPORTAMOS LOS ESTILOS PARA ESTA LISTA
+
+class BadgesList extends React.Component {
+	render() {
+		return (
+			<ul className="list-unstyled ul-BadgeList">
+				{this.props.badges.map((badge) => {
+					//el estado que lo estamos pasando como props (this.props.badges), vamos a iterar cada uno de sus elementos del objeto es un array el atributo data del estado
+					return (
+						<li key={badge.id} className="item-list-BadgeList">
+							<img
+								className="img-persona"
+								src={badge.avatarUrl}
+								alt="imagen de la persona"
+							/>
+
+							<div className="container">
+								<p className="font-weight-bold">
+									{badge.firstName} {badge.lastName}
+								</p>
+
+								<p className="p-twitter">
+									<i className="icon-twitter logo-twitter"></i> @{badge.twitter}
+								</p>
+
+								<p>{badge.jobTitle}</p>
+							</div>
+
+							<div className="wave">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+									<path
+										fill="#0099ff"
+										fillOpacity="1"
+										d="M0,64L48,64C96,64,192,64,288,101.3C384,139,480,213,576,224C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+									></path>
+								</svg>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+		);
+	}
+}
+
+export default BadgesList;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Badges from './pages/Badges'; //importamos el componente de tipo pagina Badges
 import BadgeNew from './pages/BadgeNew'; //importamos el componente BadgeNew
 import 'bootstrap/dist/css/bootstrap.css'; //importaremos bootstrap, pero hay que instalarlo con  npm install bootstrap y estos estilos los tiene el componente
 import './global.css'; //son estilos de css globales que estan en esta misma carpeta src
@@ -7,4 +9,4 @@ import './global.css'; //son estilos de css globales que estan en esta misma car
 const container = document.getElementById('app'); // obtenemos el elemento contenedor con id app
 
 // ReactDOM.render(__qué__, __dónde__) lo queremos renderizar
-ReactDOM.render(<BadgeNew />, container); // renderiza este componente con estas propiedades, Y en el componente recibe this.props.firstName y this.props.lastName y lo que pongamos aca lo pone alla
+ReactDOM.render(<Badges />, container); // renderiza este componente con estas propiedades, Y en el componente recibe this.props.firstName y this.props.lastName y lo que pongamos aca lo pone alla
